3bcf3ac3fa8188e564711fc311c28bc091a46850
Danylo Bilyi <danilo.bilyi@gmail.com>
2022-01-18 07:15:15 +0200

    add base styles and fonts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    prependData: `
+      @import 'styles/app.scss';
+    `,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
