33aa9922221d801e2e3545c1ec20bb2c6c540972
Danylo Bilyi <danilo.bilyi@gmail.com>
2022-01-19 01:12:25 +0200

    add header

---
+++ components/Header/index.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+import styles from './Header.module.scss';
+
+import Logo from 'assets/svg/logo.svg';
+import RedPaper from 'assets/img/red-paper_bottom.png';
+
+const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const links = [
+    { title: 'about us', href: '#about-us' },
+    { title: 'music', href: '#music' },
+    { title: 'concerts', href: '#concerts' },
+    { title: 'gallery', href: '#gallery' },
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`${styles.header} ${scrolled ? styles.scroll : ''}`}>
+      <div className={`container ${styles.container}`}>
+        <a href="#" className={styles.logo}>
+          <Logo />
+        </a>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            {links.map((link) => (
+              <li key={link.title}>
+                <a href={link.href} className={styles.link}>
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   sassOptions: {
     prependData: `
-      @import 'styles/app.scss';
+      @import 'styles/helpers/variables.scss';
+      @import 'styles/helpers/mixins.scss';
     `,
   },
 };
